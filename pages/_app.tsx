96ced5833b68b1d680eb6b7be317e804567098bb
Yandi <yandiriswandi@gmail.com>
2025-07-03 08:33:13 +0700

    day 2 - tailwind install

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
+import RootLayout from '@/layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+  const metaTitle =
+    router.pathname === '/' ? 'home' : router.pathname.replace('/', '')
+  return (
+    <RootLayout metaTitle={metaTitle}>
+      <Component {...pageProps} />
+    </RootLayout>
+  )
 }
